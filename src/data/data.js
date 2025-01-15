388d3ad8c958d5f5dcf49f38aea839925e90c1da
GraysonMoore <grayson.moore@ucalgary.ca>
2025-01-15 15:15:26 -0700

    grayson changed his personl info

--- src/data/data.js
+++ src/data/data.js
@@ -2,11 +2,11 @@
 
 const data = {
     name: "Grayson Moore",
-    bio: "Undergraduate Architectural Student at University of Calgary passionate about exploring different mediums within design.",
+    bio: "Undergraduate Design and City Innovation Student at University of Calgary passionate about exploring different mediums within design.",
     contact: {
         email: "grayson.moore@ucalgary.ca",
         phone: "5879174707",
-        linkedin: "https://www.linkedin.com/",
+        linkedin: "https://www.linkedin.com/GraysonMoore88",
         github: "https://www.github.com/GraysonMoore88",
     }
 }
