d4162f34ea4b8a1fa0929f5f8dfc57ff725d0195
GraysonMoore <grayson.moore@ucalgary.ca>
2025-01-19 11:33:17 -0700

    grayson changed his personl info

--- src/main.js
+++ src/main.js
@@ -38,12 +38,19 @@ document.querySelector("#app").innerHTML = `
         </div>
         <div id="images-description">
           <div id="images">
-            ${cubes
-              .map(
-                (cube, index) => `<img src="${cube}" alt="cube${index + 1}" />`
-              )
-              .join("")}
-          </div>
+  <a href="https://architecural-student-portfolio-1-hwcxrsgbb.vercel.app/cubeDrawings/cadavre1.png" target="_blank">
+    <img src="./cubeDrawings/cadavre1.png" alt="cadavre1" />
+  </a>
+  <a href="https://architecural-student-portfolio-1-hwcxrsgbb.vercel.app/cubeDrawings/cadavre2.jpg" target="_blank">
+    <img src="./cubeDrawings/cadavre2.jpg" alt="cadavre2" />
+  </a>
+  <a href="https://architecural-student-portfolio-1-hwcxrsgbb.vercel.app/cubeDrawings/cadavre3.jpg" target="_blank">
+    <img src="./cubeDrawings/cadavre3.jpg" alt="cadavre3" />
+  </a>
+  <a href="https://architecural-student-portfolio-1-hwcxrsgbb.vercel.app/cubeDrawings/Cadavre4.jpg" target="_blank">
+    <img src="./cubeDrawings/Cadavre4.jpg" alt="Cadavre4" />
+  </a>
+</div>
           <h4 id="description">We are both prisoners and architects of our pasts. Memories can both shape us and be shaped by our minds. A systematic process of picking up the pieces, pulling information from where it can, and regurgitating it into a final image. Regardless of “objective truth” the memories form our conscious and in the same manner our conscious forms our memories. The answer to this dilemma cannot be found and so it is to our own discretion to decide which narrative to believe in, neither objective, instead partial truths.</h4>
         </div>
       </div>
